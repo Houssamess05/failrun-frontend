a8206c76009838057941b2021d614ec8d83c95c0
Houssamess05 <houssam.essarhiar@gmail.com>
2026-03-08 23:21:08 +0100

    primera versió

--- failrun-frontend/src/App.vue
+++ failrun-frontend/src/App.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup>
+import '@/assets/base.css'
+
+import BarraNavegacio from './components/BarraNavegacio.vue'
+import EsloganPrincipal from './components/EsloganPrincipal.vue'
+import CarruselFails from './components/CarruselFails.vue'
+import LlistaTopFive from './components/LlistaTopFive.vue'
+import PeuPagina from './components/PeuPagina.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <BarraNavegacio />
 
-<style scoped></style>
+  <div class="main-page">
+    <EsloganPrincipal />
+
+    <div class="lower-main">
+      <div class="left-panel">
+        <CarruselFails />
+      </div>
+
+      <div class="right-panel">
+        <LlistaTopFive />
+      </div>
+    </div>
+  </div>
+
+  <PeuPagina />
+</template>
---
+++ failrun-frontend/src/components/BarraNavegacio.vue
@@ -0,0 +1,66 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="barra-navegacio">
+    <a class="nav-link" href="#">
+      <img id="Login" src="/styles/img/Profile.svg" alt="Login img">
+    </a>
+
+    <a class="nav-link" href="#">
+      <img id="Logo" src="/styles/img/Logo PLACEHOLDER.png" alt="Logo">
+    </a>
+
+    <a class="nav-button" href="#">Juegos</a>
+    <a class="nav-button" href="#">Retos</a>
+    <a class="nav-button" href="#">Ayuda</a>
+    <a class="nav-button" href="#">Sobre Nosotros</a>
+  </nav>
+</template>
+
+<style scoped>
+.barra-navegacio {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+    max-height: 60px;
+    background-color: #FF2D55;
+}
+
+#Login {
+    width: 65px;
+    height: auto;
+}
+
+#Logo {
+    width: 85px;
+    height: auto;
+}
+
+.nav-link,
+.nav-button {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 58px;
+    text-decoration: none;
+    color: white;
+    font-weight: bolder;
+}
+
+.nav-button {
+    border: none;
+    background-color: #FF2D55;
+    cursor: pointer;
+    transition: all 0.5s;
+    font-weight: 800;
+    font-size: 1.5rem;
+}
+
+.nav-button:hover {
+    background-color: #CF2949;
+}
+</style>
